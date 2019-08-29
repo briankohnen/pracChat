883da3c5b02fb91107f122e03a4ae61687214a65
BrianKohnen <briankohnen@yahoo.com>
2019-08-28 19:04:43 -0500

    bk--adding init files. wrote basic .html skeleton. initialized firebase in app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDs8CnBxteandAnYCL8-iW2vKccMQWwTuQ",
+    authDomain: "pracchat-b3755.firebaseapp.com",
+    databaseURL: "https://pracchat-b3755.firebaseio.com",
+    projectId: "pracchat-b3755",
+    storageBucket: "",
+    messagingSenderId: "162532882764",
+    appId: "1:162532882764:web:2c1a2422ee109a7d"
+  };
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
